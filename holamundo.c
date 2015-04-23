5f1fd7ebb873ae9618efe26c504ebb00f5f2b94b
Joel Leon <Jouel_898@hotmail.com>
2015-04-23 15:11:32 -0430

    Acabo de Agregar holamundo.c al repositorio

---
+++ holamundo.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <math.h>
+
+int opc;
+
+void main(){
+
+	for(opc=0;opc<=7;opc++){
+
+        	delay(1000);
+                delay(1000);
+
+	}
+
+}
